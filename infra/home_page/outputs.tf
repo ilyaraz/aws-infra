5676ca3c32c6bbf3a459e70e3e8160de6d0a0ddc
Ilya Razenshteyn <ilya.razenshteyn@gmail.com>
2025-12-31 18:18:50 -0800

    Enable 2xapply for home page

--- infra/home_page/outputs.tf
+++ infra/home_page/outputs.tf
@@ -30,7 +30,7 @@ output "acm_certificate_arn" {
 
 output "github_oidc_provider_arn" {
   description = "OIDC provider ARN for GitHub Actions."
-  value       = aws_iam_openid_connect_provider.github.arn
+  value       = data.aws_iam_openid_connect_provider.github.arn
 }
 
 output "github_actions_role_arn" {
